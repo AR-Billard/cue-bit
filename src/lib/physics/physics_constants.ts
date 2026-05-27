755fe61a766ea4d643021ee80970019c8a09426f
Lee Woo Hyun <edging-lion-unruly@duck.com>
2026-05-27 16:42:31 +0900

    chore: 병합 제대로 안된 코드 제거

--- src/lib/physics/physics_constants.ts
+++
@@ -1,6 +0,0 @@
-export const TABLE_WIDTH_M = 2.84;
-export const TABLE_HEIGHT_M = 1.42;
-export const BALL_RADIUS_M = 0.028575;
-export const BALL_MASS_KG = 0.17;
-export const GRAVITY = 9.81;
-export const POSITION_MARGIN_M = BALL_RADIUS_M + 0.005;
